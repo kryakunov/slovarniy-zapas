93f70c7513a05d02619ce305bd1c693787142588
andrey <andrey-wz@yandex.ru>
2025-10-19 14:05:58 +0500

    hot fix

--- app/Models/TgUser.php
+++ app/Models/TgUser.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class TgUser extends Model
 {
-    //
+    protected $guarded = [];
 }
--- database/migrations/2025_10_19_054302_create_tg_users_table.php
+++ database/migrations/2025_10_19_054302_create_tg_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('tg_users', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained('users')->nullable()->unique();
+            $table->foreignId('user_id')->nullable()->constrained('users');
             $table->string('tg_name');
             $table->string('tg_id');
             $table->string('tg_login');
